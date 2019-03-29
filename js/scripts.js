4d301e2c5b36e149f4d126515a9aab19c07c2e8f
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-03-29 12:57:11 -0700

    Add createIDtag()

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
+  function createIDtag(input1, input2) {
+    if (input1 === input2) {
+      return input1;
+    } else {
+      return "e"
+    }
+  };
+
+  // function buildID(input1, input2, input3) {
+  //
+  // }
+  var a = "a";
+  var b = "a";
+  var testTag = createIDtag(a,b);
+  console.log(testTag);
+
   $("form").submit(function(event) {
     event.preventDefault();
 
@@ -9,9 +25,13 @@ $(document).ready(function(){
     var q4 = $('input:radio[name=question4]:checked').val();
     var q5 = $('input:radio[name=question5]:checked').val();
     var q6 = $('input:radio[name=question6]:checked').val();
-  
+
     console.log(q1, q2, q3, q4, q5, q6);
 
+    // $('.course-card').removeClass('my-hide');
+
   });
+
+
 
 });
